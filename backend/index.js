cdce965ce5230b4767f69b316093c50503836aa5
Syeda Hafsa <144320854+tinyhafsa@users.noreply.github.com>
2025-01-15 21:27:17 +0400

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,16 +5,18 @@ import cors from 'cors';
 import userRoutes from './routes/userRoutes.js';
 dotenv.config();
 
-
 const app = express();
 const port = process.env.PORT;
 
 app.use(cors());  // Allow all origins (for debugging purposes)
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the API. Use /api/user for user routes.');
+});
 app.use('/api/user', userRoutes);
 
 const dbURI = process.env.DB_URI;
-// const dbURI = "mongodb://localhost:27017"
 
 const connectDB = async () => {
     try {
@@ -28,5 +30,5 @@ const connectDB = async () => {
 connectDB();
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
